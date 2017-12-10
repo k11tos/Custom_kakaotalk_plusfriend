d7b63b01efed6e306eff0dd821ed2421eba51569
Janghyeon Lee <k11tos@nate.com>
2017-12-10 09:05:03 +0900

    update

--- kakaotalk.py
+++ kakaotalk.py
@@ -10,7 +10,7 @@ from top_ranked_word import *
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    filename=LOG_DIR + '/log.txt',
+                    #filename=LOG_DIR + '/log.txt',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
